6d257d483cea4f38ecac0e0c26cd934eaa835180
Alid3456778 <AlidShaikh05@gmail.com>
2024-06-03 11:35:01 +0530

    adding new content to Inheritance

--- Codes/Main.java
+++ Codes/Main.java
@@ -1,11 +1,16 @@
 import java.io.*;
 import java.lang.*;
 import java.util.*;
-
+//Inheritance single inheritance
 
 //creating Super class where its function can be inherit to sub class
  class Superior{
-    String name;
+    String name="";
+
+    public Superior(){
+        System.out.println("Nothing to be shown");
+    }
+
     public Superior(String name){
         this.name=name;
     }
@@ -18,9 +23,18 @@ import java.util.*;
 //Sub class which inherit function from super class
  class Sub extends Superior{
     String bread;
+
+    public Sub(){
+        super();
+        System.out.println("No parameter Constructor ");
+        show();
+    }
+
     public Sub(String name,String bread){
-        super(name);//calling super class and calling its constructor
+        //super();//calling super class and calling its constructor
+        super(name);
         this.bread=bread;
+        display();
     }
     public void display(){
         System.out.println("Sub: "+bread);
@@ -34,7 +48,10 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
     Sub s=new Sub("Ali","Human");
-    s.display();
+    //s.display();
+
+    //Sub s1=new Sub();
+    //s1.display();
     }
 }
 
