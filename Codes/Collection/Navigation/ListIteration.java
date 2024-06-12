3eca2cb5557b8ad7f59d147487cf493174119389
Alid3456778 <AlidShaikh05@gmail.com>
2024-06-12 20:28:30 +0530

    List Itration navigation

--- Codes/Collection/Navigation/ListIteration.java
+++ Codes/Collection/Navigation/ListIteration.java
@@ -1,15 +1,39 @@
+// 1.boolean hasPrevious(): 
+// Returns true if this ListIterator has more elements when traversing the list in 
+// the reverse direction. 
+
+//2. E previous(): 
+// Returns the previous element in the list and moves the cursor position 
+// backwards. 
+// int previousIndex(): 
+// Returns the index of the element that would be returned by a subsequent call 
+// to previous(). 
+
 import java.util.*;
 
 public class ListIteration{
     public static void main(String[] args) {
         List<String> obj = new ArrayList<>();
         int n,i;
-        String name;
+        String name,sname;
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Numebr of Students");
         n=scan.nextInt();
         for(i=0;i<n;i++){
+            System.out.println("Enter the name of Student ");
             name=scan.next();
             obj.add(name);
         }
+
+        ListIterator<String> itarat=obj.listIterator();
+        System.out.println("Students name in Order");
+    while(itarat.hasNext()){
+        System.out.println(itarat.next());     
+    }
+
+    System.out.println("Students name in Reverce Order");
+    while(itarat.hasPrevious()){//it gives True when it have a previous Element
+        System.out.println(itarat.previous());//Use to Print The previous element
+    }
     }
 }
