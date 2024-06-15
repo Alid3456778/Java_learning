8f8f5af57830c3ba2840365681e4113d77352c64
Alid3456778 <AlidShaikh05@gmail.com>
2024-06-15 12:20:18 +0530

    throw

---
+++ Codes/Exception/ThrowWala.java
@@ -0,0 +1,42 @@
+import java.util.*;
+    class UserDefineException extends Exception{
+        public UserDefineException (String src){
+            super(src);
+        }
+    }
+
+    class MathsException extends Exception {
+        public MathsException(int num) {
+        System.out.println("HAve an Error");
+        }
+    }
+
+public class ThrowWala{
+
+
+
+    public static void main (String[] args){
+        //System.out.println("HEllo");
+        int a=5;
+        // try{
+        //     throw new UserDefineException("Its my User Define Exception");
+        // }
+        // catch(UserDefineException e){
+        //     System.out.println("Caught My Exception : "+e.getMessage());
+        // }
+
+        if(a<9){
+            try{
+                throw new MathsException(a);
+            }
+            catch(MathsException e){
+                System.out.println("Caught Maths Exception");
+                }
+                
+            
+        }
+        else{
+            System.out.println("It is not an error");
+        }
+    }
+}
---
+++ Codes/Exception/tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+       // try{
+        //     throw new UserDefineException("Its my User Define Exception");
+        // }
+        // catch(UserDefineException e){
+        //     System.out.println("Caught My Exception : "+e.getMessage());
+        // }
